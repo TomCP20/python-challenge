dc69269e0987268f0205519c6701d568b192cca0
dei20 <107045601+TomCP20@users.noreply.github.com>
2025-03-01 11:32:47 +0000

    solved level 22

---
+++ 22/copper.py
@@ -0,0 +1,33 @@
+"""copper"""
+from PIL import Image, ImageDraw
+
+Vec = tuple[int, int]
+
+def sub(a: Vec, b: Vec) -> Vec:
+    """returns the difference between 2 vecs"""
+    return (b[0]-a[0], b[1]-a[1])
+
+image = Image.open("white.gif")
+output = Image.new("RGB", (500, 200))
+draw = ImageDraw.Draw(output)
+x, y = 0, 100
+
+for frame in range(image.n_frames): # type: ignore
+    image.seek(frame)
+
+    box = image.getbbox()
+    assert box
+    l, u, _, _ = box
+
+    dx = l - 100
+    dy = u - 100
+
+    if dx == dy == 0:
+        x +=50
+        y = 100
+    else:
+        x += dx
+        y += dy
+    draw.point([x, y])
+
+output.show()
