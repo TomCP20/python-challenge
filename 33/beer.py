fe62beec028d1b13bc70484b34706d891f3870c7
dei20 <107045601+TomCP20@users.noreply.github.com>
2025-03-02 16:16:12 +0000

    solved level 33

---
+++ 33/beer.py
@@ -0,0 +1,28 @@
+"""beer"""
+import math
+from PIL import Image
+
+image = Image.open('beer2.png')
+
+data = list(image.getdata())
+
+
+letters: list[Image.Image] = []
+for _ in range(33):
+    max_value = max(data)
+    data = [x for x in data if x < max_value - 1]
+    l = int(math.sqrt(len(data)))
+    letter = Image.new('L', (l, l))
+    letter.putdata(data)
+    letters.append(letter)
+
+w = sum(letter.size[0] for letter in letters)
+h = letters[0].size[1]
+
+result = Image.new("L", (w, h))
+
+offset: int = 0
+for letter in letters:
+    result.paste(letter, (offset, 0))
+    offset += letter.size[0]
+result.show()
